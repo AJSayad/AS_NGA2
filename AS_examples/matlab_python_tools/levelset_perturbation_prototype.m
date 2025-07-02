c34963c7f873eb61fdc4bbde6eb9964bdd9dca2b
Andrew Sayad <asayad1@stevens.edu>
2025-07-02 16:08:24 -0400

    adjustements to level set matlab script.

--- AS_examples/matlab_python_tools/levelset_perturbation_prototype.m
+++ AS_examples/matlab_python_tools/levelset_perturbation_prototype.m
@@ -1,26 +1,28 @@
 % level set and initial perturbation prototyping
-clear; close all; clc
 
 % A level set function is a scalar field where the interface is represented as a contour of the field.
 % the sign of the funciton usually gives info about the locaiton of the point relative to the interface/contour
 % usually initialized as a signed distance funciton
 
 %% define grid
-nx = 100;
-ny = 100;
-x = linspace(-1,1,nx);
-y = linspace(-1,1,ny);
-[XX, YY] = meshgrid(x,y);
-
-%% geometry definition
 dctrx = 0; % x-center coordinate
 dctry = 0; % y-center coordinate
 dctrz = 0; % z-cetner coordinate
-d0 = 1.0;  % diameter
+d0 = 0.00187 %1.0;  % diameter
 r0 = d0/2; % radius
-epsilon = d0*[0.01, 0.007, 0.005]; % amplitude of our perturbation
-mode = [20, 11, 10];                 % wave number modes for our perturbation (spatial frequency of wave, 2pi/lambda) lambda=linear wavelength
-phase = [pi,pi,0];             % phase shifts (offset of waves)
+nx = 1000;
+ny = 1000;
+x = linspace(-2*d0,2*d0,nx);
+y = linspace(-2*d0,2*d0,ny);
+[XX, YY] = meshgrid(x,y);
+
+%% geometry definition
+epsilon = d0*[0.05, 0.0, 0.0]; % amplitude of our perturbation
+mode = [2, 0, 0];                 % wave number modes for our perturbation (spatial frequency of wave, 2pi/lambda) lambda=linear wavelength
+phase = [0,0,0];             % phase shifts (offset of waves)
+% epsilon = d0*[0.01, 0.007, 0.005]; % amplitude of our perturbation
+% mode = [20, 11, 10];                 % wave number modes for our perturbation (spatial frequency of wave, 2pi/lambda) lambda=linear wavelength
+% phase = [pi,pi,0];             % phase shifts (offset of waves)
 theta = atan2(YY-dctry,XX-dctrx);  % compute polar angle for each point (we use atan2 to keep track of the quadrant (matlab funciton) instead of just atan)
 
 % compute total perturbation based (summation of modes)
@@ -32,6 +34,15 @@ end
 % define level sets (unperturbed and perturbed)
 phi_unperturbed = sqrt(((XX - dctrx).^2) + (YY - dctry).^2) - r0;
 phi_perturbed   = sqrt(((XX - dctrx).^2) + (YY - dctry).^2) - (r0 + perturbation);
+
+% overlay the 0 level to compare
+figure
+contour(XX,YY,phi_unperturbed,[0 0],'k','LineWidth',2) % plot level set zero
+hold on
+contour(XX, YY, phi_perturbed, [0 0], 'r', 'LineWidth', 2); % perturbed interface
+axis equal; 
+xlabel('x'); ylabel('y'); legend('Unperturbed circle','perturbed circle')
+title('Comparison of level sets')
 
 % % plot unperturbed level set
 % figure;
@@ -48,13 +59,3 @@ phi_perturbed   = sqrt(((XX - dctrx).^2) + (YY - dctry).^2) - (r0 + perturbation
 % contour(XX, YY, phi_perturbed, [0 0], 'k', 'LineWidth', 2); % plot level set zero
 % axis equal; colorbar; colormap('turbo')
 % title('2D Multi-Mode Perturbed Circle (Level Set)'); xlabel('x'); ylabel('y')
-
-% overlay the 0 level to compare
-figure
-contour(XX,YY,phi_unperturbed,[0 0],'k','LineWidth',2) % plot level set zero
-hold on
-contour(XX, YY, phi_perturbed, [0 0], 'r', 'LineWidth', 2); % perturbed interface
-axis equal; 
-xlabel('x'); ylabel('y'); legend('Unperturbed circle','perturbed circle')
-title('Comparison of level sets')
-
